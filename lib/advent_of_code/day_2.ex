97487d2b3f22ddee3531496412236504b343be83
Thijs Wouters <thijs@morewood.be>
2015-12-02 12:56:55 +0100

    Solve second half of day 2

--- lib/advent_of_code/day_2.ex
+++ lib/advent_of_code/day_2.ex
@@ -2,13 +2,6 @@ defmodule AdventOfCode.Day2 do
   defmodule Package do
     defstruct [:height, :length, :width]
 
-    def paper(%Package{height: height, length: length, width: width}) do
-      first = length * height
-      second = length * width
-      third = height * width
-      2*first + 2*second + 2*third + Enum.min([first, second, third])
-    end
-
     def from_string(""), do: :error
     def from_string(string) do
       [height, length, width] = String.split(string, "x")
@@ -16,6 +9,27 @@ defmodule AdventOfCode.Day2 do
       |> Enum.map(fn {int, ""} -> int end)
 
       %Package{height: height, length: length, width: width}
+    end
+
+    def paper(package) do
+      surface(package) + Enum.min(each_surface(package))
+    end
+
+    def surface(package) do
+      Enum.reduce(each_surface(package), 0, &+/2)
+    end
+
+    defp each_surface(%Package{height: height, length: length, width: width}) do
+      surfaces = [length * height, length * width, height * width]
+      Enum.concat(surfaces, surfaces)
+    end
+
+    def ribbon(package = %Package{height: height, length: length, width: width}) do
+      2 * (height + length + width - Enum.max([height, length, width])) + volume(package)
+    end
+
+    def volume(%Package{height: height, length: length, width: width}) do
+      height * length * width
     end
   end
 
@@ -30,6 +44,20 @@ defmodule AdventOfCode.Day2 do
     parse(input)
     |> Enum.reject(&error?/1)
     |> Enum.map(&Package.paper/1)
+    |> Enum.reduce(0, &+/2)
+  end
+
+  @doc """
+  iex> ribbon("2x3x4")
+  34
+
+  iex> ribbon("1x1x10")
+  14
+  """
+  def ribbon(input) do
+    parse(input)
+    |> Enum.reject(&error?/1)
+    |> Enum.map(&Package.ribbon/1)
     |> Enum.reduce(0, &+/2)
   end
 
