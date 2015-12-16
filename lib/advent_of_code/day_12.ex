69c0c2e2efc9883b27bdc7d0d789cadf2b45d9d3
Thijs Wouters <thijs@morewood.be>
2015-12-16 20:23:00 +0100

    Solve day 12 part 2

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -8,4 +8,32 @@ defmodule AdventOfCode.Day12 do
     end)
     |> Enum.sum
   end
+
+  def sum_all_number_except_red(input) do
+    Poison.Parser.parse!(input)
+    |> sum
+  end
+
+  defp sum(list) when is_list(list) do
+    Enum.reduce(list, 0, fn element, acc ->
+      sum(element) + acc
+    end)
+  end
+
+  defp sum(string) when is_binary(string) do
+    0
+  end
+
+  defp sum(number) when is_number(number) do
+    number
+  end
+
+  defp sum(map = %{}) do
+    values = Map.values(map)
+    if Enum.member?(values, "red") do
+      0
+    else
+      sum(values)
+    end
+  end
 end
