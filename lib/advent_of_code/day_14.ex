342dd155fbedd83c04d681277c54d080a77c9663
Thijs Wouters <thijs@morewood.be>
2015-12-18 11:12:57 +0100

    Refactor for part 2

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -1,10 +1,25 @@
 defmodule AdventOfCode.Day14 do
   alias AdventOfCode.Day14.Reindeer
 
-  def winner(input, seconds) do
+  def winner(input, seconds) when is_binary(input) do
     String.split(input, "\n")
     |> Enum.map(&Reindeer.parse/1)
-    |> Enum.map(fn reindeer -> {reindeer.name, Reindeer.distance_after(reindeer, seconds)} end)
-    |> Enum.max_by(fn {_, distance} -> distance end)
+    |> winners(seconds)
+    |> Enum.map(fn reindeer ->
+      {reindeer.name, Reindeer.distance_after(reindeer, seconds)}
+    end)
+  end
+
+  def winners(reindeer, seconds) when is_list(reindeer) do
+    reindeer
+    |> Enum.filter(fn single ->
+      Reindeer.distance_after(single, seconds) == winning_distance(reindeer, seconds)
+    end)
+  end
+
+  defp winning_distance(reindeer, seconds) do
+    reindeer
+    |> Enum.map(&Reindeer.distance_after(&1, seconds))
+    |> Enum.max
   end
 end
