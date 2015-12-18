6a3e8f0575a7a8bf6cd668653fe0c4c4801d0d12
Thijs Wouters <thijs@morewood.be>
2015-12-18 11:38:47 +0100

    Solve day 14 part 2

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -1,7 +1,7 @@
 defmodule AdventOfCode.Day14 do
   alias AdventOfCode.Day14.Reindeer
 
-  def winner(input, seconds) when is_binary(input) do
+  def winners(input, seconds) when is_binary(input) do
     String.split(input, "\n")
     |> Enum.map(&Reindeer.parse/1)
     |> winners(seconds)
@@ -15,6 +15,17 @@ defmodule AdventOfCode.Day14 do
     |> Enum.filter(fn single ->
       Reindeer.distance_after(single, seconds) == winning_distance(reindeer, seconds)
     end)
+  end
+
+  def winners_on_points(input, seconds) do
+    reindeer = String.split(input, "\n")
+    |> Enum.map(&Reindeer.parse/1)
+
+    1..seconds
+    |> Enum.map(fn seconds -> winners(reindeer, seconds) end)
+    |> List.flatten
+    |> Enum.group_by(fn reindeer -> reindeer.name end)
+    |> Enum.map(fn {key, value} -> {key, length(value)} end)
   end
 
   defp winning_distance(reindeer, seconds) do
