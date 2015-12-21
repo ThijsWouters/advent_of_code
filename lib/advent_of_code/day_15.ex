b63a9b5af4a6122809aa75f124aaf27d083d5b43
Thijs Wouters <thijs@morewood.be>
2015-12-21 16:32:02 +0100

    Refactor

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -3,10 +3,14 @@ defmodule AdventOfCode.Day15 do
   alias AdventOfCode.Day15.Cookie
 
   def best_cookie(input, total) do
+    recipes(input, total)
+    |> Enum.map(fn cookie -> {cookie, Cookie.score(cookie)} end)
+    |> Enum.max_by(&elem(&1, 1))
+  end
+
+  defp recipes(input, total) do
     String.split(input, "\n")
     |> Enum.map(&Ingredient.parse/1)
     |> Cookie.build(total)
-    |> Enum.map(fn cookie -> {cookie, Cookie.score(cookie)} end)
-    |> Enum.max_by(&elem(&1, 1))
   end
 end
