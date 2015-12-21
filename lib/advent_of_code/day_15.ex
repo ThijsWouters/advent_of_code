0a0ebaf866cc92ed7200e133c03b29a0e3979808
Thijs Wouters <thijs@morewood.be>
2015-12-21 16:37:25 +0100

    Solve day 15 part 2

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -8,6 +8,13 @@ defmodule AdventOfCode.Day15 do
     |> Enum.max_by(&elem(&1, 1))
   end
 
+  def best_cookie(input, total, calories) do
+    recipes(input, total)
+    |> Enum.filter(fn cookie -> Cookie.property_score(cookie, :calories) == 500 end)
+    |> Enum.map(fn cookie -> {cookie, Cookie.score(cookie)} end)
+    |> Enum.max_by(&elem(&1, 1))
+  end
+
   defp recipes(input, total) do
     String.split(input, "\n")
     |> Enum.map(&Ingredient.parse/1)
