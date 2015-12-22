52f67a2a0773bd80ee8bdbcb953a4c9412c94841
Thijs Wouters <thijs@morewood.be>
2015-12-22 12:30:29 +0100

    Solve day 17 part 2

--- lib/advent_of_code/day_17.ex
+++ lib/advent_of_code/day_17.ex
@@ -4,6 +4,14 @@ defmodule AdventOfCode.Day17 do
     |> length
   end
 
+  def minimum_containers(input, eggnog) do
+    different_combinations(input, eggnog)
+    |> Enum.group_by(fn combination -> Enum.count(combination) end)
+    |> Enum.min_by(fn {key, _} -> key end)
+    |> elem(1)
+    |> Enum.count
+  end
+
   def all_combinations([]), do: []
   def all_combinations(list = [_]), do: [list]
   def all_combinations([head|rest]) do
